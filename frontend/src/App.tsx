e2607c917984b9a3e711afcd6a7dc42031402297
Reza-namvaran <reza.namvaran823@gmail.com>
2025-10-01 16:33:44 +0330

    (feat): Add basic react structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1 className='font-mono font-bold underline text-blue-400 dark:text-sky-400 text-3xl'>Hello, FP!</h1>
+    </>
+  )
+}
+
+export default App
