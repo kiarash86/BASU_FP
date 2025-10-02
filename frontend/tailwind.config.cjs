fa1c01ef6a3655c07092922e693cfd3b381e0736
Reza-namvaran <reza.namvaran823@gmail.com>
2025-10-02 13:20:34 +0330

    (refactor): Add custom configs

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,11 +1,137 @@
 /** @type {import('tailwindcss').Config} */
+const { fontFamily } = require("tailwindcss/defaultTheme");
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        color: {
+          1: "#AC6AFF",
+          2: "#FFC876",
+          3: "#FF776F",
+          4: "#7ADB78",
+          5: "#858DFF",
+          6: "#FF98E2",
+        },
+        stroke: {
+          1: "#26242C",
+        },
+        n: {
+          1: "#FFFFFF",
+          2: "#CAC6DD",
+          3: "#ADA8C3",
+          4: "#757185",
+          5: "#3F3A52",
+          6: "#252134",
+          7: "#15131D",
+          8: "#0E0C15",
+          9: "#474060",
+          10: "#43435C",
+          11: "#1B1B2E",
+          12: "#2E2A41",
+          13: "#6C7275",
+        },
+      },
+      fontFamily: {
+        sans: ["var(--font-sora)", ...fontFamily.sans],
+        code: "var(--font-code)",
+        grotesk: "var(--font-grotesk)",
+      },
+      fontSize: {
+        h1: ["2.5rem", { lineHeight: "3.25rem" }],
+        h2: ["2rem", { lineHeight: "2.5rem" }],
+        h3: ["1.75rem", { lineHeight: "2.25rem" }],
+      },
+      letterSpacing: {
+        tagline: ".15em",
+      },
+      spacing: {
+        0.25: "0.0625rem",
+        7.5: "1.875rem",
+        15: "3.75rem",
+      },
+      opacity: {
+        15: ".15",
+      },
+      transitionDuration: {
+        DEFAULT: "200ms",
+      },
+      transitionTimingFunction: {
+        DEFAULT: "linear",
+      },
+      zIndex: {
+        1: "1",
+        2: "2",
+        3: "3",
+        4: "4",
+        5: "5",
+      },
+      borderWidth: {
+        DEFAULT: "0.0625rem",
+      },
+      backgroundImage: {
+        "radial-gradient": "radial-gradient(var(--tw-gradient-stops))",
+        "conic-gradient":
+          "conic-gradient(from 225deg, #FFC876, #79FFF7, #9F53FF, #FF98E2, #FFC876)",
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addBase, addComponents, addUtilities }) {
+      addComponents({
+        ".container": {
+          "@apply mx-auto px-5 md:px-10 lg:px-15": {},
+          maxWidth: "77.5rem",
+        },
+        "@screen xl": {
+          ".container": {
+            maxWidth: "87.5rem",
+          },
+        },
+        ".h1": {
+          "@apply font-semibold text-h1": {},
+        },
+        ".h2": {
+          "@apply text-h2": {},
+        },
+        ".h3": {
+          "@apply text-h3": {},
+        },
+        ".h4": {
+          "@apply text-2xl": {},
+        },
+        ".h5": {
+          "@apply text-xl": {},
+        },
+        ".h6": {
+          "@apply font-semibold text-lg": {},
+        },
+        ".body-1": {
+          "@apply text-base": {},
+        },
+        ".body-2": {
+          "@apply font-light text-sm": {},
+        },
+        ".caption": {
+          "@apply text-sm": {},
+        },
+        ".tagline": {
+          "@apply font-grotesk font-light text-xs tracking-tagline uppercase": {},
+        },
+        ".quote": {
+          "@apply font-code text-lg": {},
+        },
+        ".button": {
+          "@apply font-code text-xs font-bold uppercase tracking-wider": {},
+        },
+      });
+      addUtilities({
+        ".tap-highlight-color": {
+          "-webkit-tap-highlight-color": "rgba(0, 0, 0, 0)",
+        },
+      });
+    }),
+  ],
 };
