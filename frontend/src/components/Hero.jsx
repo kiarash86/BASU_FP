9a194019f511a43ce08a57cec1f2c653b4a00a64
Reza-namvaran <reza.namvaran823@gmail.com>
2025-10-03 22:43:17 +0330

    (feat): Add background moving circles

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -39,6 +39,8 @@ const Hero = () => {
                     <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
                     <img src={heroBackground} className="w-full" width={1440} height={1800} alt="heroBG"/>
                 </div>
+
+                <BackgroundCircles />
                 </div> 
         </Section>        
     );
