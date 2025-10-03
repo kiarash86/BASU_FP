11a1903fcf7d0c70d91a9b4a8690ce47872cb5b1
Reza-namvaran <reza.namvaran823@gmail.com>
2025-10-03 22:32:52 +0330

    (feat): Add picture and text to hero

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,24 +1,41 @@
-import { curve } from "../assets";
+import { curve, heroBackground, code, codeMobile } from "../assets";
 import Button from "./Button";
 import Section from "./Section";
-
+import { BackgroundCircles, BottomLine, Gradient } from "./design/Hero";
+import { heroIcons } from "../constants";
+import { ScrollParallax } from "react-just-parallax";
+import { useRef } from "react";
 
 const Hero = () => {
+    // const parallaxRef = useRef(null);
+
     return (
         <Section className="pt-[12rem] -mt-[5.25rem]" crossess crossessOffset="lg:translate-y-[5.25rem]" customPaddings id="hero">
             <div className="container relative">
                 <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb:[6.25rem]">
                     <h1 className="h1 mb-6">
+                        Your first step into the world of
                         <span className="inline-block relative">
-                            Fundamentals of Programming
+                            Programming
                             <img src={curve} className="absolute py-2 top-full left-0 w-full xl:-mt-2" width={624} height={28}/>
                         </span>
                     </h1>
 
-                    <p className="body-1 max-w-3xl py-5 mx-auto mb-6 text-n-2 lg:mb-8">A clear path to help you in fundamentals of programming and computer course in Bu-Ali Sina university</p>
-                    <Button href="/pricing" white>Fall 2025</Button>
+                    <p className="body-1 max-w-3xl py-5 mx-auto mb-6 text-n-2 lg:mb-8">A clear path to help you in fundamentals of programming course in Bu-Ali Sina University</p>
+                    <Button className="cursor-default" href="/#'" white>Fall 2025</Button>
                 </div>
             </div>
+
+            <div className="relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24">
+                <div className="relative z-1 p-0.5 rounded-2xl bg-conic-gradient">
+                    <div className="relative bg-n-8 rounded-[1rem]">
+                        <div className="h-[1.4rem] bg-n-10 rounded-t-[0.9rem]" />
+                        <div className="aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]">
+                            <img src={code} className="w-full scale-[1.7] translate-y-[8%] md:scale-[1] md:-translate-y-[10%] lg:-translate-y-[23%]" width={1024} height={490} alt="Main picture" />
+                        </div>
+                        </div>
+                    </div>
+                </div> 
         </Section>        
     );
 };
