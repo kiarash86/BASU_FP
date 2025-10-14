21cb344c5d3635df7cb21080154aacd5c0eda061
Reza-namvaran <reza.namvaran823@gmail.com>
2025-10-14 18:29:06 +0330

    (refactor): Improve styling for roadmap

--- frontend/src/components/Roadmap.jsx
+++ frontend/src/components/Roadmap.jsx
@@ -9,15 +9,15 @@ const RoadmapEmbed = () => {
       crossessOffset="lg:translate-y-[5.25rem]"
       customPadding="py-20"
     >
-      
-    <div className="roadmap-dark-wrapper relative w-full h-[800px] overflow-hidden rounded-xl">
-      <iframe
-        src="https://roadmap.sh/r/embed?id=68e654b31d72874d102728f1"
-        title="FP Roadmap"
-        className="absolute top-0 left-0 w-full h-full border-0"
-      ></iframe>
-    </div>
-
+      <div className="w-full max-w-[100vw] md:max-w-[500vw] lg:max-w-[500vw] rounded-xl overflow-hidden">
+        <div className="relative" style={{ paddingTop: "56.25%" }}>
+          <iframe
+            src="https://roadmap.sh/r/embed?id=68e654b31d72874d102728f1"
+            title="FP Roadmap"
+            className="absolute top-0 left-0 w-full h-full border-0"
+          ></iframe>
+        </div>
+      </div>
     </Section>
   );
 };
