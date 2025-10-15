469a279c9faa1760a9b7974ede64d6d01fda2205
Reza-namvaran <reza.namvaran823@gmail.com>
2025-10-15 18:08:44 +0330

    (feat): main HTTP server setup

--- backend/internal/server/server.go
+++ backend/internal/server/server.go
@@ -1,1 +1,21 @@
 package server
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+type Server struct {
+	Router *chi.Mux
+}
+
+func New() *Server {
+	return &Server{Router: chi.NewRouter()}
+}
+
+func (s *Server) Start(host string, port int) error {
+	addr := fmt.Sprintf("%s:%d", host, port)
+	return http.ListenAndServe(addr, s.Router)
+}
