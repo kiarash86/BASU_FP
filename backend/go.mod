c0c90afa03c3537c3d32ca269a48d0a0d2281afe
Reza-namvaran <reza.namvaran823@gmail.com>
2025-10-16 23:45:06 +0330

    (feat): Add CORS middleware
    Still in dev state

--- backend/go.mod
+++ backend/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
 	github.com/aymerick/douceur v0.2.0 // indirect
 	github.com/go-chi/chi/v5 v5.2.3 // indirect
+	github.com/go-chi/cors v1.2.2 // indirect
 	github.com/golang-jwt/jwt/v5 v5.3.0 // indirect
 	github.com/gorilla/css v1.0.1 // indirect
 	github.com/ilyakaznacheev/cleanenv v1.5.0 // indirect
---
+++ backend/internal/middleware/cors.go
@@ -0,0 +1,18 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/go-chi/cors"
+)
+
+func CORS() func(http.Handler) http.Handler {
+		return cors.Handler(cors.Options{
+			AllowedOrigins:   []string{"*"}, // TODO: restrict in stable state
+			AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+			AllowedHeaders:   []string{"Authorization", "Content-Type"},
+			AllowCredentials: false,
+			MaxAge:           300,
+		})
+	}
+}
